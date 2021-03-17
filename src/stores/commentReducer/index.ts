3cee4777d5fdee0169830095983a97c04331bac0
octobrekids <chnrd.k@gmail.com>
2021-03-17 10:57:46 +0700

    add commentReducer

---
+++ src/stores/commentReducer/index.ts
@@ -0,0 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { initCommentState } from "./init";
+
+const commentSlice = createSlice({
+    name: 'comment',
+    initialState: initCommentState,
+    reducers: {}
+})
