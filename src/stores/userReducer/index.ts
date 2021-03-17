29bfbe10dd653a8c0ff7064cf07abb1fd05049d4
octobrekids <chnrd.k@gmail.com>
2021-03-17 10:54:50 +0700

    add userReducer

---
+++ src/stores/userReducer/index.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { initUserState } from "./init";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: initUserState,
+  reducers: {
+    setUser: (
+      state,
+      action: PayloadAction<{ loggedIn: boolean; user: {} }>
+    ) => {
+      const { payload } = action;
+      state.loggedIn = true;
+      state.user = payload.user;
+    },
+    logout: (state, action) => {
+      localStorage.clear();
+      state.loggedIn = false;
+      state.user = {};
+    },
+  },
+});
+
+export const { setUser } = userSlice.actions;
