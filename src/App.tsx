a6a18c5737b241bd058014348f4156ce99b0ae6b
octobrekids <chnrd.k@gmail.com>
2021-04-17 21:54:19 +0700

    Add SignUp and Login Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
-
+import LoginComponent from './components/Login';
+import SignUpComponent from './components/SignUp';
+import { StoresState } from './stores';
+import { autoLogin } from './stores/service/userClient';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(autoLogin());
+	}, []);
+
+	const loggedIn = useSelector((state: StoresState) => state.user.loggedIn);
+	const user = useSelector((state: StoresState) => state.user.user);
+
+	return (
+		<div className="App">
+			{!loggedIn ? <h1>sign up or login</h1> : <h1> welcome, {user} </h1>}
+			<SignUpComponent />
+			<LoginComponent />
+			<button>Logout</button>
+		</div>
+	);
 }
 
 export default App;
