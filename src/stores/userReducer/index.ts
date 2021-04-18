955062b613123e39cd31f5f72b2ff1b3879b3f83
octobrekids <chnrd.k@gmail.com>
2021-04-19 00:59:37 +0700

    add signup featchuer and autologin to reducer

--- src/stores/userReducer/index.ts
+++ src/stores/userReducer/index.ts
@@ -1,26 +1,38 @@
-import { createEntityAdapter, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { initUserState } from "./init";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { autoLogin, fetchUser, signUserUp } from './action';
+import { initUserState } from './init';
 
 const userSlice = createSlice({
-  name: "user",
-  initialState: initUserState,
-  reducers: {
-    setUser: (
-      state,
-      action: PayloadAction<{ loggedIn: boolean; user: {} }>
-    ) => {
-      const { payload } = action;
-      state.loggedIn = true;
-      state.user = payload.user;
-    },
-    logout: (state, action) => {
-      localStorage.clear();
-      state.loggedIn = false;
-      state.user = {};
-    },
-  },
+	name: 'user',
+	initialState: initUserState,
+	reducers: {
+		setUser: (
+			state,
+			action: PayloadAction<{ loggedIn: boolean; user: {} }>
+		) => {
+			const { payload } = action;
+			state.loggedIn = true;
+			state.user = payload.user;
+		},
+		logout: (state) => {
+			localStorage.removeItem('token');
+			state.loggedIn = false;
+			state.user = {};
+		},
+	},
+	extraReducers: (builder) => {
+		builder.addCase(signUserUp.fulfilled, (state, action) => {
+			localStorage.setItem('token', action.payload.accessToken);
+		});
+		builder.addCase(fetchUser.fulfilled, (state, action) => {
+			localStorage.setItem('token', action.payload.accessToken);
+		});
+		builder.addCase(autoLogin.fulfilled, (state, action) => {
+			localStorage.setItem('token', action.payload.accessToken);
+			state.loggedIn = true;
+		});
+	},
 });
-
 export const { setUser, logout } = userSlice.actions;
 
 export default userSlice.reducer;
