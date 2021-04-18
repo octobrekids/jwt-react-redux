3392a645b39311abd2b6015199d2227e07c17947
octobrekids <chnrd.k@gmail.com>
2021-04-18 20:29:11 +0700

    refactor

---
+++ src/stores/userReducer/action.ts
@@ -0,0 +1,23 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { ISignUp } from '../../components/SignUp';
+
+export const autoLogin = createAsyncThunk('user/autoLogin', async () => {
+	const URL = 'http://localhost:4000/login';
+	const config = {
+		headers: {
+			Authorization: `Bearer ${localStorage.getItem('token')}`,
+		},
+	};
+	const res = await axios.get(URL, config);
+	return res.data;
+});
+
+export const signUserUp = createAsyncThunk(
+	'user/signUserUp',
+	async (userInfo: ISignUp) => {
+		const URL = 'http://localhost:4000/login';
+		const res = await axios.post(URL, userInfo);
+		return res.data;
+	}
+);
